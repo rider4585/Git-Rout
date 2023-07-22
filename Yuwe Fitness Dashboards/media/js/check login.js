405d7b5736ee112b7688391f63d82bed571385f7
Oasiz-admin <rider4585>
2023-07-23 05:09:22 +0530

    updated: login check logic

--- Yuwe Fitness Dashboards/media/js/check login.js
+++ Yuwe Fitness Dashboards/media/js/check login.js
@@ -1,3 +1,3 @@
 if(!sessionStorage.getItem('loggedIn')){
-    window.location.href = "/Yuwe Fitness Dashboards/Dashboard Login/login.html";
+    window.location.href = "./Dashboard Login/login.html";
 }
