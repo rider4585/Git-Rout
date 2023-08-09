a84cb5665b9036bc16a47f80cee4ad1dd5ce74a1
Oasiz-admin <rider4585>
2023-08-10 00:52:52 +0530

    updated : service worker

--- Task Tracker/service-worker.js
+++ Task Tracker/service-worker.js
@@ -1,21 +1,46 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('your-app-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/index.css',
-          '/index.js'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+// Service Worker script (sw.js)
+
+const CACHE_NAME = 'your-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.js'
+  // Add more URLs to cache here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        return response; // Serve from cache
+      }
+
+      // Fetch from network and cache the response
+      return fetch(event.request)
+        .then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response; // Don't cache non-200 responses or non-basic types
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+    })
+  );
+});
