cd2214be0e43f8f1657b268fdfd1d16c1bbff5ed
Oasiz-admin <rider4585>
2023-10-08 01:31:14 +0530

    updated : inquiry form ui and added setData

--- Yuwe Fitness Dashboards/media/js/inquiry.js
+++ Yuwe Fitness Dashboards/media/js/inquiry.js
@@ -1,41 +1,33 @@
-// Define a FormValidator class
 class FormValidator {
     constructor(formId, alertContainerId) {
         this.form = document.getElementById(formId);
         this.alertContainer = document.getElementById(alertContainerId);
     }
 
-    // Initialize form validation
     initialize() {
         this.form.addEventListener('submit', event => {
             event.preventDefault();
             if (this.validateForm()) {
-                const formData = this.getFormData();
-                console.log('Form submitted successfully!');
-                console.log(formData);
-                this.sendDataToFlutterFunction(JSON.stringify(formData));
-                this.form.reset();
+                this.getFormData();
             }
         });
     }
 
-    // Display an alert message
     showAlert(message) {
         this.alertContainer.innerHTML = `
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        ${message}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                `;
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                ${message}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        `;
         const closeButton = this.alertContainer.querySelector('.close');
         closeButton.addEventListener('click', () => {
             this.alertContainer.innerHTML = '';
         });
     }
 
-    // Validate the form fields
     validateForm() {
         let valid = true;
         const name = this.form.elements.name.value.trim();
@@ -45,7 +37,6 @@ class FormValidator {
         const goals = this.form.elements.goals.value;
         const preferredTime = this.form.elements.preferredTime.value;
 
-        // Name validation
         if (name === '') {
             valid = false;
             this.form.elements.name.classList.add('is-invalid');
@@ -57,7 +48,6 @@ class FormValidator {
             this.form.elements.name.classList.remove('is-invalid');
         }
 
-        // Email validation
         if (email === '') {
             valid = false;
             this.form.elements.email.classList.add('is-invalid');
@@ -65,7 +55,6 @@ class FormValidator {
             this.form.elements.email.classList.remove('is-invalid');
         }
 
-        // Phone number validation
         if (phone === '') {
             valid = false;
             this.form.elements.phone.classList.add('is-invalid');
@@ -77,7 +66,6 @@ class FormValidator {
             this.form.elements.phone.classList.remove('is-invalid');
         }
 
-        // Gender validation
         if (gender === '') {
             valid = false;
             this.form.elements.gender.classList.add('is-invalid');
@@ -85,7 +73,6 @@ class FormValidator {
             this.form.elements.gender.classList.remove('is-invalid');
         }
 
-        // Preferred Time validation
         if (preferredTime === '') {
             valid = false;
             this.form.elements.preferredTime.classList.add('is-invalid');
@@ -96,14 +83,13 @@ class FormValidator {
         return valid;
     }
 
-    // Get form data as JSON object
     getFormData() {
         const checkboxes = this.form.querySelectorAll('input[type="checkbox"]:checked');
         const services = Array.from(checkboxes).map(checkbox => checkbox.value);
 
         const sendUpdateToWhatsApp = this.form.elements.sendUpdate.checked;
 
-        return {
+        const dataToSend = {
             name: this.form.elements.name.value.trim(),
             email: this.form.elements.email.value.trim(),
             phone: `+91${this.form.elements.phone.value.trim()}`,
@@ -113,18 +99,56 @@ class FormValidator {
             goal: this.form.elements.goals.value,
             sendUpdateToWhatsApp: sendUpdateToWhatsApp ? true : false
         };
+
+        this.sendDataToFlutterFunction(JSON.stringify(dataToSend));
+        this.form.reset();
     }
 
-    // Send form data to Flutter
+    setData(data) {
+        const servicesContainer = this.form.querySelector('#services-checkbox');
+
+        // Clear any existing checkboxes
+        servicesContainer.innerHTML = '';
+
+        // Create checkboxes for each service in the data
+        data.services.forEach(service => {
+            const serviceItem = document.createElement('div');
+            serviceItem.className = 'form-check form-check-inline service-item';
+
+            const checkbox = document.createElement('input');
+            checkbox.className = 'form-check-input';
+            checkbox.type = 'checkbox';
+            checkbox.id = service.toLowerCase(); // Use a lowercase ID for consistency
+
+            const label = document.createElement('label');
+            label.className = 'form-check-label';
+            label.htmlFor = service.toLowerCase(); // Match the ID
+
+            label.textContent = service;
+
+            serviceItem.appendChild(checkbox);
+            serviceItem.appendChild(label);
+
+            servicesContainer.appendChild(serviceItem);
+        });
+    }
+
+
+
     sendDataToFlutterFunction(formData) {
-        SendDataToFlutter.postMessage(formData)
+        SendDataToFlutter.postMessage(formData);
     }
 }
 
-// Create an instance of FormValidator and initialize it
 const formValidator = new FormValidator('enquiryForm', 'alertContainer');
 formValidator.initialize();
 
 function isThisYuWeWebPage() {
     return true;
 }
+
+let dummyData = {
+    "services": ["Yoga", "Zumba"]
+}
+
+formValidator.setData(dummyData);
