54c615aa6dc13846ca23e34bb0af737e01f13220
rider4585 <ravirajbugge4585@gmail.com>
2024-11-01 00:22:08 +0530

    fixed : manifest file issues

--- SpendWise/media/js/index.js
+++ SpendWise/media/js/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
